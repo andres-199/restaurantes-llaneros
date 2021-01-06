3df41254eb2c8e8c583def9d71979839f4e624db
andres199 <calopez@asertisas.com>
2021-01-06 12:01:46 -0500

    https options added

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,36 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from '@nestjs/core'
+import { AppModule } from './app.module'
+import { Logger } from '@nestjs/common'
+import * as fs from 'fs'
+
+const logger = new Logger('Main')
+let httpsOptions: any
+try {
+  httpsOptions = {
+    key: fs.readFileSync(
+      '/etc/letsencrypt/live/restaurantesllaneros.com/privkey.pem'
+    ),
+    cert: fs.readFileSync(
+      '/etc/letsencrypt/live/restaurantesllaneros.com/fullchain.pem'
+    )
+  }
+  logger.log('Leyendo certificado SSL')
+} catch (error) {
+  logger.log('No se pudo encontrar el certificado SSL')
+}
 
 async function bootstrap() {
-  const volleyball = require('volleyball');
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const volleyball = require('volleyball')
+  const app = await NestFactory.create(AppModule, { cors: true, httpsOptions })
 
   app.use(async (req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Accept');
-    next();
-  });
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Accept')
+    next()
+  })
 
-  app.use(volleyball);
-  await app.listen(3001);
+  app.use(volleyball)
+  await app.listen(3001)
 }
-bootstrap();
+bootstrap()
--- src/public/restaurantes/restaurantes.entity.ts
+++ src/public/restaurantes/restaurantes.entity.ts
@@ -2,39 +2,34 @@ import { Table, Model, Column, DataType, HasMany } from 'sequelize-typescript'
 import { Direccion } from '../direcciones/direcciones.entity'
 
 @Table({
-	schema: 'public',
-	tableName: 'restaurantes',
+  schema: 'public',
+  tableName: 'restaurantes'
 })
-
 export class Restaurant extends Model<Restaurant> {
-
-	@Column({
-  	type: DataType.INTEGER,
-  	autoIncrement: true,
-  	primaryKey: true,
-  	unique: true,
-  	field: 'id',
-	})
-	id: number
-
-
+  @Column({
+    type: DataType.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+    unique: true,
+    field: 'id'
+  })
+  id: number
 
   @Column
-	nombre: string
+  nombre: string
 
   @Column
-	descripcion: string
+  descripcion: string
 
   @Column
-	createdAt: Date
+  createdAt: Date
 
   @Column
-	updatedAt: Date
+  updatedAt: Date
 
-
+  @Column
+  imagen: string
 
   @HasMany(() => Direccion, { as: 'Direccion', foreignKey: 'propietario_id' })
-	Direccion: Direccion[]
-
-
+  Direccion: Direccion[]
 }
